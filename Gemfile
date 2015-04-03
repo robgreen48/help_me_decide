9e8c7fd72524a36d5cbfff3e16bfae356aa9c990
Rob Green <rob@trustedhousesitters.com>
2015-04-03 10:45:20 +0100

    fix for pg on dev

--- Gemfile
+++ Gemfile
@@ -36,7 +36,7 @@ group :development, :test do
   gem 'byebug'
 
   # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'pg'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150330192639) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "parties", force: :cascade do |t|
     t.string   "name"
     t.string   "leader"
